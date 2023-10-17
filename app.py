7c687cca9df6460f6719d0093778279ff8e3b79b
Diana <papkavadiana@gmail.com>
2023-10-17 21:39:08 +0300

    HW(Lesson13.10)

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import os
-from flask import Flask, render_template
-from dotenv import load_dotenv
+from flask import Flask
+
 
 app = Flask(__name__)
 
-load_dotenv()
+
 @app.route("/")
 def main():
-    return render_template(".index.html")
+    return "My hobby is programming and target"
 
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv("DEBUG"))
+    app.run()
