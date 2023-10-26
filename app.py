4aeacae3d2733bad1259eb15278d50d81529d275
Diana <papkavadiana@gmail.com>
2023-10-26 19:30:44 +0300

    Lesson 24.10

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
+from enums import MAX_SCORE, NAME, students
 
+load_dotenv()
 
 app = Flask(__name__)
 
 
-load_dotenv()
-
-
-@app.route("/hobby")
+@app.route("/")
 def main():
     return render_template("index.html")
+@app.route("/base")
+def base():
+    return render_template("base.html", title="Python course")
+
+
+@app.route("/context")
+def context():
+    context_dict = {
+        "title": "Python Course",
+        "name": NAME,
+        "max_score": MAX_SCORE,
+        "students": students
+    }
+    return render_template("context.html", **context_dict)
 
 
 if __name__ == "__main__":
