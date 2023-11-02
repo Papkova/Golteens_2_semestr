e6ccb906e6c402dcd8315e670ac58222177d1add
Diana <papkavadiana@gmail.com>
2023-11-02 17:56:59 +0200

    Lesson 31.10

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
-from enums import MAX_SCORE, NAME, students
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_BINDS'] = {
+    'db': os.getenv("SQLALCHEMY_DATABASE_URI"),
+}
 
+db = SQLAlchemy(app)
 
-@app.route("/")
-def main():
-    return render_template("index.html")
 @app.route("/base")
 def base():
     return render_template("base.html", title="Python course")
-
-
-@app.route("/context")
-def context():
-    context_dict = {
-        "title": "Python Course",
-        "name": NAME,
-        "max_score": MAX_SCORE,
-        "students": students
-    }
-    return render_template("context.html", **context_dict)
 
 
 if __name__ == "__main__":
