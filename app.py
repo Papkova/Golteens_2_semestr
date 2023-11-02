2183e1d39e8c334bfd8bb2663abc17a122b2c5e9
Diana <papkavadiana@gmail.com>
2023-11-02 18:21:38 +0200

    Дз 17.10

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
+
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
-app.config['SQLALCHEMY_BINDS'] = {
-    'db': os.getenv("SQLALCHEMY_DATABASE_URI"),
-}
 
-db = SQLAlchemy(app)
-
-@app.route("/base")
-def base():
-    return render_template("base.html", title="Python course")
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
