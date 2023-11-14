6c1eefc0ff6f6446905e18779ba82d23f83b3520
Diana <papkavadiana@gmail.com>
2023-11-14 18:43:04 +0200

    Дз 07.11

--- models.py
+++ models.py
@@ -1,23 +1,23 @@
-#from app import app
-#from datetime import datetime
-#from dotenv import load_dotenv
-#from flask_sqlalchemy import SQLAlchemy
-
-#load_dotenv()
-
-#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///diana.db"
-##app.config['SQLALCHEMY_BINDS'] = {
-#    'db': "sqlite:///diana.db",
-#}
-#db = SQLAlchemy(app)
-
-
-#class Article(db.Model):
- #   id = db.Column(db.Integer, primary_kay=True)
- #   title = db.Column(db.Srting(100), nullable=False)
- #   intro = db.Column(db.Srting(100), nullable=False)
-  #  text = db.Column(db.Text, nullablr=False)
-   # data = db.Column(db.DateTime, default=datetime.utcnow)
-
-  #  def __repr__(self):
-  #      return f'<Article {self.id}>'
+# import os
+# from app import app
+# from datetime import datetime
+# from dotenv import load_dotenv
+# from flask_sqlalchemy import SQLAlchemy
+#
+# load_dotenv()
+#
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///diana.db"
+# app.config['SQLALCHEMY_BINDS'] = {
+#     'db': "sqlite:///diana.db",
+# }
+# db = SQLAlchemy(app)
+#
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50), unique=True, nullable=False)
+#     password_hash = db.Column(db.String(128), nullable=False)
+#     articles = db.relationship('Article', backref='author', lazy=True)
+#
+#     def __repr__(self):
+#         return f'<User {self.id}>'
