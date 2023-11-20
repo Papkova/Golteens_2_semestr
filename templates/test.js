b56dc4c56ac174417d744e4890b1a71522a1a18e
Diana <papkavadiana@gmail.com>
2023-11-20 22:07:33 +0200

    Дз 20.11

--- templates/test.js
+++ templates/test.js
@@ -1,6 +1,28 @@
 window.onload = function () {
-    let username = document.getElementById("zmina");
-    let username_ = prompt("What is your name");
-    let result= "Name: " + username_;
-    username.innerHTML = result;
+    let password = document.getElementById("zmina");
+    let password_ = prompt("Enter your password");
+    let result= "Password: " + password_;
+    password.innerHTML = result;
 }
+
+// window.onload =function () {
+//     onload_window_zmina = document.getElementById("zmina")
+//     console.log(onload_window_zmina)
+//     onload_window_zmina.innerHTML = "<h1>Hello world!!!<h1/>"
+// }
+//
+// // window.onload = function (){
+// //     let username= document.getElementById("zmina");
+// //     let age= prompt("Скільки тобі років");
+// //     let years= age * 2;
+// //     let result= "Число" + years;
+// //     username.innerHTML = result;
+// // }
+//
+// window.onload = function (){
+//     let password= document.getElementById("zmina_1");
+//     let age= prompt("Enter your password");
+//     let years= age * 2;
+//     let result= "Число" + years;
+//     password.innerHTML = result;
+// }
