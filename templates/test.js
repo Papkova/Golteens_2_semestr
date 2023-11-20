13e4d35d84ed0a00a2d8b6cbb889db85aebf365d
Diana <papkavadiana@gmail.com>
2023-11-20 22:04:26 +0200

    Дз 20.11

--- templates/test.js
+++ templates/test.js
@@ -1,13 +1,6 @@
-// window.onload =function () {
-//     onload_window_zmina = document.getElementById("zmina")
-//     console.log(onload_window_zmina)
-//     onload_window_zmina.innerHTML = "<h1>Hello world!!!<h1/>"
-// }
-
-window.onload = function (){
-    let onload_window_zmina= document.getElementById("zmina");
-    let age= prompt("Скільки тобі років");
-    let years= age * 2;
-    let result= "Число" + years;
-    onload_window_zmina.innerHTML = result;
+window.onload = function () {
+    let username = document.getElementById("zmina");
+    let username_ = prompt("What is your name");
+    let result= "Name: " + username_;
+    username.innerHTML = result;
 }
